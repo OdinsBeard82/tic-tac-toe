1a283575a7c567ee3c621f40b1f9996caa078370
“OdinsBeard82 <danielcharles_500@hotmail.com>
2023-04-03 08:38:37 +0100

    eventlisters added for player select

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-function Gameboard {
+function Gameboard() {
+  this.board = [
+   ['', '', ''],
+   ['', '', ''],
+   ['', '', ''],
+  ];
+}
 
+function Player(name, symbol) {
+ this.name = name;
+ this.symbol = symbol;
+}
+
+const player1 = new Player('Player 1', 'X');
+const player2 = new Player('Player 2', 'O');
+
+const player1Button = document.querySelector('.player1');
+const player2Button = document.querySelector('.player2');
+player1Button.addEventListener('click', selectPlayer);
+player2Button.addEventListener('click', selectPlayer);
+
+function selectPlayer(event) {
+ const selectedPlayer = event.target.classList[0];
+ if (selectedPlayer === 'player1') {
+   console.log(player1.name + ' has selected symbol ' + player1.symbol);
+ } else if (selectedPlayer === 'player2') {
+   console.log(player2.name + ' has selected symbol ' + player2.symbol);
+ }
 }
