581e03912a77e9e1999be64bb7c2ea35a2c8c835
“OdinsBeard82 <danielcharles_500@hotmail.com>
2023-04-25 14:06:13 +0100

    functionality complete, just needs more CSS

--- script.js
+++ script.js
@@ -4,68 +4,124 @@ const gameBoard= {
     ['', '', ''],
     ['', '', ''],
   ]
-}
-
-const Player1 = {
-  name: 'Player1',
-  symbol: 'X'
 };
 
-const Player2 = {
-  name: 'Player2',
-  symbol: 'O'
-};
-
-let currentPlayers = [];
+let currentPlayerIndex = 0;
 
 function Player(name, symbol) {
   this.name = name;
   this.symbol = symbol;
 }
 
+
+const player1Button = document.querySelector('.player1');
+const player2Button = document.querySelector('.player2');
+const restartButton = document.querySelector('.restart');
+const cells = document.querySelectorAll('.cell');
+
+const winningSelections = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+let currentPlayers = [];
+
 function addPlayerToGame(name, symbol) {
   const newPlayer = new Player(name, symbol);
   currentPlayers.push(newPlayer);
 }
 
-addPlayerToGame('Player1', 'X');
-addPlayerToGame('Player2', 'O');
+function startGame() {
+  const player1Name = document.getElementById('player1').value;
+  const player2Name = document.getElementById('player2').value;
+  
+  if (player1Name !== '' && player2Name !== '') {
+    addPlayerToGame(player1Name, 'X');
+    addPlayerToGame(player2Name, 'O');
+  }
+}
 
-let currentPlayerIndex = 0;
-const player1 = currentPlayers[0];
-const player2 = currentPlayers[1];
-
-const player1Button = document.querySelector('.player1');
-const player2Button = document.querySelector('.player2');
-player1Button.addEventListener('click', selectPlayer);
-player2Button.addEventListener('click', selectPlayer);
+document.getElementById('start-button').addEventListener('click', startGame);
 
 function selectPlayer(event) {
   const selectedPlayer = event.target.classList[0];
   if (selectedPlayer === 'player1') {
     currentPlayerIndex = 0;
-    console.log(player1.name + ' has selected symbol ' + player1.symbol);
   } else if (selectedPlayer === 'player2') {
     currentPlayerIndex = 1;
-    console.log(player2.name + ' has selected symbol ' + player2.symbol);
   }
 }
 
-function cellSelection(event) {
+function addSymbolToBoard(event) {
   const selectedCell = event.target;
   const rowIndex = selectedCell.dataset.row;
   const colIndex = selectedCell.dataset.col;
   const currentPlayer = currentPlayers[currentPlayerIndex];
 
-  if (selectedCell.textContent === '') {
-    selectedCell.textContent = currentPlayer.symbol;
-    gameBoard.board[rowIndex][colIndex] = currentPlayer.symbol;
-    checkForWin();
-    checkForDraw();
-    currentPlayerIndex = (currentPlayerIndex + 1) % currentPlayers.length;
-  }
+  gameBoard.board[rowIndex][colIndex] = currentPlayer.symbol;
+
+  selectedCell.textContent = currentPlayer.symbol;
+  currentPlayerIndex = (currentPlayerIndex +1) % currentPlayers.length;
+
+  CheckForWin(cells);
 }
 
-const cells = document.querySelectorAll('.cell');
-cells.forEach(cell => cell.addEventListener('click', cellSelection));
+function CheckForWin(cells) {
+  let filledCells = 0;
 
+  for (let win of winningSelections) {
+    if(cells[win[0]].textContent == cells[win[1]].textContent
+      && 
+      cells[win[1]].textContent == cells[win[2]].textContent
+      &&
+      cells[win[0]].textContent !=''
+      ){
+        const winnerSymbol = cells[win[0]].textContent;
+        const winner = currentPlayers.find(player => player.symbol === winnerSymbol).name || `Player ${winnerSymbol}`;
+        const congratulationMsg = `Congratulations ${winner}! You won!`;
+        const winningMsg = document.createElement('p');
+        winningMsg.textContent = congratulationMsg;
+        document.body.appendChild(winningMsg);
+        return true;
+      }
+  }
+  for (let cell of cells) {
+    if (cell.textContent !== '') {
+      filledCells++;
+    }
+  }
+
+  if (filledCells === cells.length) {
+        const congratulationMsg = `It's a tie!`;
+        const winningMsg = document.createElement('p');
+        winningMsg.textContent = congratulationMsg;
+        document.body.appendChild(winningMsg);
+    return true;
+  }
+  return false;
+}
+
+
+function restartGame() {
+  gameBoard.board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ];
+  cells.forEach(cell => cell.textContent = '');
+  currentPlayerIndex = 0;
+}
+
+addPlayerToGame('Player1', 'X');
+addPlayerToGame('Player2', 'O');
+
+player1Button.addEventListener('click', selectPlayer);
+player2Button.addEventListener('click', selectPlayer);
+cells.forEach(cell => cell.addEventListener('click', addSymbolToBoard));
+restartButton.addEventListener('click', restartGame);
